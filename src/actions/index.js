08644a7b96ca1531d3b178518263e641e65ab5fb
emin <mustafazadeemin2000@gmail.com>
2021-05-16 12:22:07 +0400

    main

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,13 @@
-import JsonPlaceHolder from '../apis/JsonPlaceholder';
+import JsonPlaceholder from '../apis/JsonPlaceholder';
 
 export const fetchPosts = () => async (dispatch) => {
-        const response = await JsonPlaceHolder.get('/posts')
+        const response = await JsonPlaceholder.get('/posts')
 
-        dispatch({type: 'FETCH_POSTS', payload: response})
-    };
+        dispatch({ type: 'FETCH_POSTS', payload: response.data })
+};
+
+export const fetchUser = id => async (dispatch) => {
+    const response = await JsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data })
+};
--- src/apis/JsonPlaceholder.js
+++ src/apis/JsonPlaceholder.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseIRL: 'https://jsonplaceholder.typicode.com',
+    baseURL: 'https://jsonplaceholder.typicode.com',
 })
